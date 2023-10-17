ba9dffc07609fe4fbda3a31cf800665d385c6bd5
iamcarinn <karina825karpenkova@yandex.ru>
2023-10-17 17:53:55 +0300

    Task 1

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,24 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+// расшифровка полей с суффиксом id
+function decodeFields(data) {
+    const decoded = data.map(item => {
+        const decodedItem = {...item};
+        for (let key in decodedItem) {
+            if (key.endsWith('Id') && key !== 'groupId' && key !== 'service' && key !== 'formatSize' && key !== 'ca') {
+                const translationKey = decodedItem[key];
+                decodedItem[key] = translations[translationKey] || translationKey;
+            }
+        }
+        return decodedItem;
+    });
+    return decoded;
+}
 
+const decoded = decodeFields(encoded);
+console.log(decoded);
 
-// console.log(decoded)
+// список уникальных id
+const uniqueIds = [...new Set(encoded.flatMap(item => Object.values(item)))];
+console.log(uniqueIds);
